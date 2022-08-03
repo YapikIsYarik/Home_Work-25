b59822e81d608531527edfe494bb27a839c7e9e4
THEyarik <yaroslavpatcalon@gmail.com>
2022-08-03 09:02:38 +0300

    add basename URL

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import {HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter   basename="/Home_Work-25">
+      <HashRouter>
           <App />
       </HashRouter>
-
   </React.StrictMode>
 );
 
